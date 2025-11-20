37f1abe2b06a5286d2a09ddf56314e7b28bd2781
sher128t <85415604+sher128t@users.noreply.github.com>
2025-11-20 19:58:13 +0000

    resolve @/lib/api

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,7 +1,13 @@
 /** @type {import('next').NextConfig} */
+const path = require("path");
+
 const nextConfig = {
-  experimental: {
-    serverActions: true,
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      "@": path.resolve(__dirname),
+    };
+    return config;
   },
 };
 
